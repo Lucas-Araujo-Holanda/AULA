92a6543c3bda8737944ea54f9e221547099363f9
Lucas Araújo <lucas.araujo.holanda@gmail.com>
2020-05-16 12:43:10 -0300

    adiciona regrade marcacao de tarefa feita

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+let todoItems = [ ];
+
+//funcao adiciona todo
+function addTodo(text) {
+
+    const todo = {
+        text,
+        checked: false,
+        id: Date.now(),
+    }
+
+    todoItems.push(todo);
+    console.log('LISTA - ', todoItems);
+
+    //Referencia ul no html
+    const list = document.querySelector('.js-todo-list')
+
+    list.insertAdjacentHTML('beforeend', `
+        <li class="todo-item" data-key ="${todo.id}">    
+                <input id="${todo.id}" type= "checkbox"/>
+
+                <label for= "${todo.id}" class= "tick js-tick"></label>
+                
+                <span>${todo.text}</span>
+                <button class="delete-todo js-delete-todo">
+                    <svg>
+                        <use href="#delete-icon"></use>
+                    </svg>
+                </button>
+        </li>
+    `)
+}
+
+//verifica evento formulario e valor input
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const input = document.querySelector('.js-todo-input');
+
+    var text = input.value.trim();
+    if(text !== ' ') {
+        addTodo(text);
+        input.value = ' ';
+        input.focus();
+    }
+})
+
+//check to-do
+function checkTodo(key) {
+    const index = todoItems.findIndex(item  => item.id === Number(key))
+    todoItems [index].checked = !todoItems[index].checked;
+
+    const item = document.querySelector(`[data-key='${key}'']`)
+    if(todoItems[index].checked) {
+        item.classList.add('done');
+    } else {
+        item.classList.remove('done');
+    }
+}
+
+
+//verifica click e item da lista para checar
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+    if (event.target. classList.contains('js-tick')){
+        const itemKey =event.target.parentElement.dataset.key;
+        console.log('chave do elemento', itemKey);
+        checkTodo(key)    
+        }
+
+})
